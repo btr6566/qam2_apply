389020616a5de4bc5589346b3818c9cbcd439795
btroberts6566 <ben.roberts6566@gmail.com>
2024-06-29 18:41:00 +0100

    Added Logoto Navbar

--- decisioning_dashboard/src/components/navBar.jsx
+++ decisioning_dashboard/src/components/navBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import vbgLogo from '../images/Vanquis Banking Group Horizontal Logo Mark RGB_WHITE.jpg';
 
 function NavBar() {
     return ( 
         <nav>
+            {/* Logo roughly 12.5% of original size */}
+            <img src={vbgLogo} width={238} height={75} alt="logo" />
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/summary-data">Summary Data</Link></li>
