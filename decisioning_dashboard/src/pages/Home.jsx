db919e09e2f729732b128ad15eb5827e4871bd1e
Benjamin Roberts <benjamin.roberts@vanquis.com>
2024-07-05 17:28:29 +0100

    Hello world from backend working + Starts both ends
    Starts both Front end React & Back end Node.js

--- decisioning_dashboard/src/pages/Home.jsx
+++ decisioning_dashboard/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 
 
 //SQL Connection
+
+// Can't do directly, need a proper backend to Node.js
+
 // import "crypto-browserify";
 // import "web-streams-polyfill/polyfill"; //For resolving issues in mssql import
 // const readable = new ReadableStream();
-
-import mssql from 'mssql'
+// import mssql from 'mssql'
 
 
 
@@ -20,10 +22,24 @@ import styles from './styles/Home.module.css'
 
 
 function Home () {
+
+
+    //Loads into state
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
+
+
+
     return (  
         <div className={styles.app}>
             <header className={styles.appHeader}>
-                <p>Hello World!</p>
+                {/* <p>Hello World!</p> */}
+                <p>{!data ? "Loading..." : data}</p>
             </header>
         </div>
     );
