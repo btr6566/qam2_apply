5351e3629dfd92675d47271fb0816c1bb47fd36e
Benjamin Roberts <benjamin.roberts@vanquis.com>
2024-07-15 10:29:54 +0100

    Fetch Backend added on Home.jsx

--- decisioning_dashboard/src/pages/Home.jsx
+++ decisioning_dashboard/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 
 
 
@@ -21,25 +21,34 @@ import React from 'react'
 import styles from './styles/Home.module.css'
 
 
+//Backend
+const mostRecentURL = 'http://localhost:3001/test';
+
 function Home () {
 
 
     //Loads into state
-    const [data, setData] = React.useState(null);
+    const [data, setData] = useState('');
 
-    React.useEffect(() => {
-        fetch("/api")
+    //UseEffect causes React to re-render when promise is fuffilled?
+    useEffect(() => {
+        fetch(mostRecentURL)
             .then((res) => res.json())
             .then((data) => setData(data.message));
     }, []);
 
+    //Log to Condole once data changes
+    useEffect( () => {
+        console.log(data)
+    }, [data])
 
 
     return (  
         <div className={styles.app}>
             <header className={styles.appHeader}>
                 {/* <p>Hello World!</p> */}
-                <p>{!data ? "Loading..." : data}</p>
+                {/* <p>{!data ? "Loading..." : data}</p> */}
+                <p>{data}</p>
             </header>
         </div>
     );
